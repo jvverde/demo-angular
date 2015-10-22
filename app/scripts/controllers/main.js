08313e996cbba13dbe15f412c00e2d30115db677
Joao Araujo <jvilaverde@yahoo.com>
2015-10-22 16:26:37 +0100

    editable

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,10 +10,17 @@
 angular.module('demoApp')
   .controller('MainCtrl', function($scope,$http) {
     $scope.title = 'Lista de Clientes';
+    var editIndex = undefined;
     $http
       .get('./data/customers.json')
       .then(function(response){
         $scope.customers = response.data;    
       })
     ;
+    $scope.edit = function(index){
+      editIndex = index;  
+    }
+    $scope.editable = function(index){
+      return index === editIndex;
+    }
   });
