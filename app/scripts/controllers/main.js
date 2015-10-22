45d1e27c87c73e6fed6761551b8706ea2bfa141b
Joao Araujo <jvilaverde@yahoo.com>
2015-10-22 15:42:40 +0100

    http

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,24 +8,12 @@
  * Controller of the demoApp
  */
 angular.module('demoApp')
-  .controller('MainCtrl', function($scope) {
+  .controller('MainCtrl', function($scope,$http) {
     $scope.title = 'Lista de Clientes';
-    $scope.customers = [
-      {
-        name: 'Rita',
-        amount: 120.3
-      },{
-        name: 'Luís',
-        amount: 20
-      },{
-        name: 'Paula',
-        amount: 76
-      },{
-        name: 'Helena',
-        amount: 87.003
-      },{
-        name: 'Belmiro',
-        amount: 140.05
-      }
-    ];
+    $http
+      .get('./data/customers.json')
+      .then(function(response){
+        $scope.customers = response.data;    
+      })
+    ;
   });
